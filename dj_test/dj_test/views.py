dd101d725cdafad0701bf8c71007175e1295e1d7
nunungrustendi01 <nunungrustendi01@gmail.com>
2024-09-17 20:23:04 +0700

    rev4

--- dj_test/dj_test/views.py
+++ dj_test/dj_test/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.core.cache import cache
+import logging
 
 def index(request):
 #------------------
-    ckey="DSnhits465"
-    nhits=cache.get(ckey,0)
-    nhits+=1
-    cache.set(ckey, nhits)
+    nhits=0
+    try:
+        ckey="DSnhits465"
+        nhits=cache.get(ckey,0)
+        nhits+=1
+        cache.set(ckey, nhits)
+    except Exception as e:
+        logging.warning(f"Exception : {str(e)}")
     context={'nhits':nhits}
     return render(request,"index.html",context)
 
